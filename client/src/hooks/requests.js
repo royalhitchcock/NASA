6f2aa53cf6787e554fa83343a26c6f8827fb2c5a
that guy <trey.hitchcock@gmail.com>
2022-05-19 22:32:48 -0700

    First Commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,31 @@
+const API_URL = 'http://localhost:8000';
+
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`)
+  return await response.json();
+}
+
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 8000;
+
+const http = require('http');
+
+const {loadPlanetsData} = require('./models/planets.model');
+
+const app = require('./app');
+
+const server = http.createServer(app);
+
+async function startServer() {
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer();
